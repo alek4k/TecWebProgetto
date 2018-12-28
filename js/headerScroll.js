959f01babafcf6fb24b44bbaeaed4888a2c64bfc
Alessandro Lovo <alessandro.lovo@studenti.unipd.it>
2018-12-28 11:37:14 +0100

    menu mobile

--- js/headerScroll.js
+++ js/headerScroll.js
@@ -1,12 +1,40 @@
 $(document).ready(function() {
 
+    navHeaderSettings();
+
     $(document).scroll(function() {
         var y = $(document).scrollTop();
         var header = $("#navbar");
-        if(y >= $("#topHeader").height())  {
-            header.css({position: "fixed", "top" : "0"});
-        } else {
-            header.css("position", "static");
+        if ($(window).width() > 600) {
+            if (y >= $("#topHeader").height()) {
+                header.css({position: "fixed", "top": "0"});
+            } else {
+                header.css("position", "static");
+            }
+        }
+        else {
+            header.css({position: "fixed", "top": "0"});
         }
     });
+
+    $('#hamburger-menu').on("click",function(){
+        console.log("dai");
+        $('#navbar').toggleClass("visible");
+    })
+
 });
+
+$(window).on('resize', function() {
+    navHeaderSettings();
+});
+
+function navHeaderSettings() {
+    if ($(window).width() <= 600) {
+        $("#navbar").css("padding-top", $("#topHeader").height());
+        $("#navbar").css("position", "fixed");
+    }
+    else {
+        $("#navbar").css("padding-top", "0");
+        $("#navbar").css("position", "static");
+    }
+}
